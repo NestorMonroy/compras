59162c6284d9e29154b361cf47a1d25c6ba6fb1d
NestorMonroy <nestor.monroy.90@gmail.com>
2020-09-26 23:07:00 -0500

    adding Subcategoria new

--- app/inv/forms.py
+++ app/inv/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db.models import fields
 from django.forms import widgets
 
-from .models import Categoria
+from .models import Categoria, SubCategoria
 
 
 class CategoriaForm(forms.ModelForm):
@@ -19,3 +19,22 @@ class CategoriaForm(forms.ModelForm):
                 self.fields[field].widget.update({
                     'class':'form-control'
                 })
+
+
+class SubCategoriaForm(forms.ModelForm):
+    class Meta:
+        model = SubCategoria
+        fields = [ 'categoria' ,'descripcion', 'estado']
+        labels = { 'descripcion':'Descripcion de la  Sub Categoria',
+                'estado':'Estado'}
+        widgets={'descripcion': forms.TextInput}
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args,*kwargs)
+            for field in iter(self.fields):
+                self.fields[field].widget.update({
+                    'class':'form-control'
+                })
+                
+            self.fields['categoria'].empty_label = "Selecione Categoria"
+
