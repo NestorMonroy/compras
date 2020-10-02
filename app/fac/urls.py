7dbe9be898b500ae423aa973aafb0b6419fa0f7e
NestorMonroy <nestor.monroy.90@gmail.com>
2020-10-02 01:06:38 -0500

    adding report view

--- app/fac/urls.py
+++ app/fac/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import ClienteView, ClienteNew, ClienteEdit, clienteInactivar, FacturaView , \
     facturas, ProductoView, borrar_detalle_factura
 
+from .reportes import imprimir_factura_recibo, imprimir_factura_list
 
 
 urlpatterns = [
@@ -15,6 +16,10 @@ urlpatterns = [
     path('facturas/edit/<int:id>',facturas, name="factura_edit"),
     path('facturas/buscar-producto',ProductoView.as_view(), name="factura_producto"),
     path('facturas/borrar-detalle/<int:id>',borrar_detalle_factura, name="factura_borrar_detalle"),
+
+    path('facturas/imprimir/<int:id>',imprimir_factura_recibo, name="factura_imprimir_one"),
+    path('facturas/imprimir-todas/<str:f1>/<str:f2>',imprimir_factura_list, name="factura_imprimir_all"),
+
 
 
 ]
